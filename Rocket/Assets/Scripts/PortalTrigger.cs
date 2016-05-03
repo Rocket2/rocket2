82787dbac4b6b263551e3f0b45a684d8ef3a34f9
mauro <mbazan@plus54.com>
2016-05-03 10:23:32 -0300

    Adding new repo

---
+++ Rocket/Assets/Scripts/PortalTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PortalTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+
+
+
+    void OnTriggerEnter2D(Collider2D colliderInfo)
+    {
+        if (colliderInfo.tag == "Player")
+        {
+            //Fade in, fade out.
+            float fadeTime = GameObject.Find("_GM").GetComponent<FadeScene>().BeginFade(1);
+            Debug.Log("Trigger");
+            GameManager.ChangeLevel(fadeTime);
+           
+        }
+
+    }
+
+
+}
